2b3a1140c6a64668891a8b574ec45e63da23c8d7
Lucas LeBlanc <lucas.leblanc@mths.ca>
2022-09-15 15:13:20 +0000

    init

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Lucas LeBlanc
+# Created on: Sep 2022
+# This is the "Hello, World!" program, with proper style
+
+def main():
+    # this function prints to the console
+
+    print("Hello, World!")
+
+    print("\nDone.")
+
+if __name__ == "__main__":
+    main()
