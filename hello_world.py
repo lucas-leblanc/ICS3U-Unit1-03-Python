1e9042a25350868bbbe36e67b22639c34be818ec
Lucas LeBlanc <lucas.leblanc@mths.ca>
2022-09-16 14:25:31 +0000

    fix linting errors

--- hello_world.py
+++ hello_world.py
@@ -4,12 +4,14 @@
 # Created on: Sep 2022
 # This is the "Hello, World!" program, with proper style
 
+
 def main():
     # this function prints to the console
 
     print("Hello, World!")
 
     print("\nDone.")
+
 
 if __name__ == "__main__":
     main()
